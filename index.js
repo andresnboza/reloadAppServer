d1ab74854a988a700eac0b337ca22d27befea574
andres.navarrete <andres.navarrete@ibm.com>
2022-07-05 13:18:22 -0600

    update

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-import express from "express";
-import bodyParser from "body-parser";
-import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import readmeRoutes from "./modules/readme/readme.routes.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import readmeRoutes from './modules/readme/readme.routes.js';
 
-import "dotenv/config";
+import cors from "cors";
+
+import 'dotenv/config';
+
+const corsOptions = {
+  origin: true,
+  methods: ['GET'],
+  optionsSuccessStatus: 200 // For legacy browser support
+}
 
 connectDB()
-  .then(() => {
-    const app = express();
-    const port = process.env.PORT || 3000;
+	.then(() => {
+		const app = express();
+		const port = process.env.PORT || 3000;
 
-    app.use(bodyParser.json());
+		app.use(cors(corsOptions));
+		app.use(bodyParser.json());
 
-    app.use("/api/readme", readmeRoutes);
+		app.use('/api/readme', readmeRoutes);
 
-    app.get("/", (req, res) => {
-      res.send("Hello World");
-    });
+		app.get('/', (req, res) => {
+			res.send('Hello World');
+		});
 
-    app.use(notFound);
-    app.use(errorHandler);
+		app.use(notFound);
+		app.use(errorHandler);
 
-    app.listen(port, () => {
-      console.log(`Server listening on the port  ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error(`Error: ${error.message}`.red.underline.bold);
-  });
+		app.listen(port, () => {
+			console.log(`Server listening on the port  ${port}`);
+		});
+	})
+	.catch((error) => {
+		console.error(`Error: ${error.message}`.red.underline.bold);
+	});
