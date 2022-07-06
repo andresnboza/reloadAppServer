30054d9b0fdd79ae199502fddcd670b8d276e305
andres.navarrete <andres.navarrete@ibm.com>
2022-07-05 18:18:16 -0600

    update

--- modules/readme/readme.controller.js
+++ modules/readme/readme.controller.js
@@ -1,9 +1,14 @@
-import asyncHandler from "express-async-handler";
-import Readme from "./readme.model.js";
+import asyncHandler from 'express-async-handler';
+import Readme from './readme.model.js';
 
 const getAllUploads = asyncHandler(async (req, res) => {
-  const items = await Readme.find({});
-  res.json({ data: items });
+	const items = await Readme.find({});
+
+	items.map((item) => {
+		console.log('item', item);
+	});
+
+	res.json({ data: items });
 });
 
 export { getAllUploads };
